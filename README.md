de08a4ca4537020ed4b5a1b1e2ac145475565ead
Sara Petre <sara.petre@solidify.dev>
2023-04-19 16:00:44 +0200

    first commit with files

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# github_wiki_create_the_first_page_with_selenium_1.0
+# Python script to be able to automatly create the first wiki page in Github.
+
+# The script will:
+* Navigate to github.com
+* Log in to your account with:
+    -username
+    -github password
+* Go into settings and uncheck botton "Restrict editing to collaborators only"
+* Navigate to wiki-tab in main github window
+* Find "create the first page"-tab and click it
+* Find "save"-tab and click it.
+
+
+Done!! Your wiki is now ready to go!
+
+## Set up
+# Create a virtual environment
+Creating a Virtual Environment with WSL2 and Linux on Windows 11
+1. Installing WSL2. Using the command line
+* Open a command prompt as an Administrator and run the following line of code:
+>wsl --install
+2. Download Ubuntu
+>wsl --list --online
+* Choose the Ubunto version you want to install. Here we install Ubuntu-22.04
+>wsl --install -d Ubuntu-22.04
+* If you want to check which distros are installed in the WSL and what version of WSL they are running on you can check by typing:
+>wsl -l -v
+* install the latest updates by typing in the terminal:
+>sudo apt update
+>sudo apt full-upgrade
+* To create the new python environment type this code in the Ubuntu terminal:
+>python3 -m venv saras_venv (choose your venv name. Here its saras_venv)
+* Activate your venv.
+>source <venv>/bin/activate
+
+# Install requirements
+> pip3 install -r requirements.txt
+
+# Run script
+> python3 wiki_selenium_script.py
+
+# After work is done
+* Deactivate your venv
+> deactivate
---
+++ wiki_selenium_script.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+def create_first_wiki_page():
+
+    #Github credentials
+    username = "github username"
+    password = "github password"
+    project_name= "project (repo)name"
+
+    # initialize the Chrome driver
+    driver = webdriver.Chrome()
+
+    # head to github login page
+    driver.get("https://github.com/login")
+
+    # find username/email field and send the username itself to the input field
+    driver.find_element("id", "login_field").send_keys(username)
+    
+    # find password input field and insert password as well
+    driver.find_element("id", "password").send_keys(password)
+    
+    # click login button
+    driver.find_element("name", "commit").click()
+    time.sleep(5)
+    
+    # wait the ready state to be complete
+    WebDriverWait(driver=driver, timeout=10).until(
+        lambda x: x.execute_script("return document.readyState === 'complete'") 
+    )
+    driver.get(f"https://github.com/{username}/{project_name}")
+
+    # Maximize size of window
+    driver.maximize_window()
+    time.sleep(5)
+
+    # find setting tab and click on it
+    settings =driver.find_element(By.ID, "settings-tab").click()
+    time.sleep(5)
+
+    # find "Restrict editing to collaborators only" under Features/Wikis and unmark
+    restrict_edit= driver.find_element(By.XPATH, "/html/body/div[1]/div[6]/div/main/turbo-frame/div/div/div[2]/div/div/div/div[4]/form[1]/div[1]/div/div/label").click()
+    
+    # Navigate to wiki-tab and click on it
+    clickable_wiki = driver.find_element(By.ID, "wiki-tab").click()
+    time.sleep(5)
+
+    # Navigate to "create first page" and click on it
+    clickable_Createfirstpage = driver.find_element(By.XPATH, "//html/body/div[1]/div[6]/div/main/turbo-frame/div/div/div/a/span/span").click()
+    time.sleep(5)
+
+    # find "save" and click on it.
+    clickable_savepage = driver.find_element(By.XPATH, "/html/body/div[1]/div[6]/div/main/turbo-frame/div/div/div[2]/div/form/div[4]/button").click()
+    time.sleep(5)
+create_first_wiki_page()
